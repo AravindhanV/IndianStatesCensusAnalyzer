d044542a6f721ae0cb5eaeec0c4e92547c56e7e1
Aravindhan V <aravindhan.v@perfios.com>
2021-09-21 21:12:49 +0530

    ADDED: contructor to CensusAnalyzerException

--- src/main/java/com/bridgelabz/indianstatescensusanalyzer/CensusAnalyzerException.java
+++ src/main/java/com/bridgelabz/indianstatescensusanalyzer/CensusAnalyzerException.java
@@ -1,5 +1,16 @@
 package com.bridgelabz.indianstatescensusanalyzer;
 
-public class CensusAnalyzerException {
-	
+public class CensusAnalyzerException extends RuntimeException {
+	enum ExceptionType{
+		CENSUS_FILE_PROBLEM,
+		UNABLE_TO_PARSE,
+		CENSUS_INCORRECT_FILE_FORMAT
+	}
+
+	ExceptionType type;
+
+	 public CensusAnalyzerException(String message, ExceptionType type) {
+		 super(message);
+	     this.type = type;
+	   }
 }
